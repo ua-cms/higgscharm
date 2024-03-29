90404fa9038414ed7bbe0c2ae2ba790f59bd7c7a
deoache <daniel.ocampoh@udea.edu.co>
2024-03-29 14:35:08 +0000

    fix path

--- analysis/configs/dataset/2022EE/DYto2L_2Jets_0J.py
+++ analysis/configs/dataset/2022EE/DYto2L_2Jets_0J.py
@@ -5,7 +5,7 @@ dataset_config = DatasetConfig(
     path=(
         "root://maite.iihe.ac.be:1094//store/user/daocampo/PFNano_Run3/"
         "mc_summer22EE_MINIAODv4/DYto2L-2Jets_MLL-50_0J_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/"
-        "Run3Summer22EEMiniAODv4-130X_mcRun3_2022_realistic_postEE_v6-v2_BTV_Run3_2022_Comm_MINIAODv4/240326_081304/0000"
+        "Run3Summer22EEMiniAODv4-130X_mcRun3_2022_realistic_postEE_v6-v2_BTV_Run3_2022_Comm_MINIAODv4/240326_081304/0000/"
     ),
     key="Events",
     year="2022EE",
