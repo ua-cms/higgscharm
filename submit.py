b9809df29477418f28d03d2e2cdaf89d8df5dbfe
deoache <daniel.ocampoh@udea.edu.co>
2024-04-16 14:39:16 +0000

    update submitter

--- submit.py
+++ submit.py
@@ -2,25 +2,27 @@ import json
 import dask
 import pickle
 import argparse
-from coffea.nanoevents import PFNanoAODSchema
+from coffea.nanoevents import NanoEventsFactory, PFNanoAODSchema
 from analysis.processors.tag_eff import TaggingEfficiencyProcessor
-from coffea.dataset_tools import apply_to_fileset, max_chunks
+
 
 def main(args):
+    # load fileset and get PFNano events array
+    with open(args.fileset_path) as f:
+        fileset = json.load(f)[args.dataset_name]
+    events = NanoEventsFactory.from_root(
+        fileset["files"],
+        schemaclass=PFNanoAODSchema,
+        metadata={"dataset": args.dataset_name},
+    ).events()
+
+    # set processor
     processors = {"tag_eff": TaggingEfficiencyProcessor}
     p = processors[args.processor](tagger=args.tagger, wp=args.wp, flavor=args.flavor)
 
-    with open(args.dataset_runnable) as f:
-        dataset_runnable = json.load(f)
-
-    to_compute = apply_to_fileset(
-        p,
-        max_chunks(dataset_runnable, None),
-        schemaclass=PFNanoAODSchema,
-        #uproot_options={"allow_read_errors_with_report": True}
-    )
+    # compute and save output
+    to_compute = p.process(events)
     (computed,) = dask.compute(to_compute)
-
     with open(
         f"{args.output_path}/{args.dataset_name}.pkl", "wb"
     ) as handle:
@@ -44,11 +46,11 @@ if __name__ == "__main__":
         help="dataset name",
     )
     parser.add_argument(
-        "--dataset_runnable",
-        dest="dataset_runnable",
+        "--fileset_path",
+        dest="fileset_path",
         type=str,
         default="",
-        help="dataset runnable",
+        help="fileset path",
     )
     parser.add_argument(
         "--year",
