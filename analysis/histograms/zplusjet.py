07809433370b286d64829585e1e04a0fc74c238b
deoache <daniel.ocampoh@udea.edu.co>
2024-05-17 15:52:24 +0000

    add systematic axis

--- analysis/histograms/zplusjet.py
+++ analysis/histograms/zplusjet.py
@@ -2,7 +2,7 @@ import hist
 import numpy as np
 import hist.dask as hda
 
-
+syst_axis = hist.axis.StrCategory([], name="variation", growth=True)
 z_mass_axis = hist.axis.Regular(
     bins=100, start=10, stop=150, name="z_mass", label=r"$m(Z)$ [GeV]"
 )
@@ -31,13 +31,13 @@ deltaphi_axis = hist.axis.Regular(
 n_jets_axis = hist.axis.IntCategory(categories=np.arange(0, 16), name="njets")
 n_vertices_axis = hist.axis.Regular(bins=60, start=0, stop=60, name="npvs")
 histograms = {
-    "z_mass": hda.hist.Hist(z_mass_axis, hist.storage.Weight()),
-    "mu1_pt": hda.hist.Hist(mu1_pt_axis, hist.storage.Weight()),
-    "mu2_pt": hda.hist.Hist(mu2_pt_axis, hist.storage.Weight()),
-    "cjet_pt": hda.hist.Hist(jet_pt_axis, hist.storage.Weight()),
-    "cjet_eta": hda.hist.Hist(jet_eta_axis, hist.storage.Weight()),
-    "cjet_phi": hda.hist.Hist(jet_phi_axis, hist.storage.Weight()),
-    "cjet_z_deltaphi": hda.hist.Hist(deltaphi_axis, hist.storage.Weight()),
-    "njets": hda.hist.Hist(n_jets_axis, hist.storage.Weight()),
-    "npvs": hda.hist.Hist(n_vertices_axis, hist.storage.Weight()),
+    "z_mass": hda.hist.Hist(z_mass_axis, syst_axis, hist.storage.Weight()),
+    "mu1_pt": hda.hist.Hist(mu1_pt_axis, syst_axis, hist.storage.Weight()),
+    "mu2_pt": hda.hist.Hist(mu2_pt_axis, syst_axis, hist.storage.Weight()),
+    "cjet_pt": hda.hist.Hist(jet_pt_axis, syst_axis, hist.storage.Weight()),
+    "cjet_eta": hda.hist.Hist(jet_eta_axis, syst_axis, hist.storage.Weight()),
+    "cjet_phi": hda.hist.Hist(jet_phi_axis, syst_axis, hist.storage.Weight()),
+    "cjet_z_deltaphi": hda.hist.Hist(deltaphi_axis, syst_axis, hist.storage.Weight()),
+    "njets": hda.hist.Hist(n_jets_axis, syst_axis, hist.storage.Weight()),
+    "npvs": hda.hist.Hist(n_vertices_axis, syst_axis, hist.storage.Weight()),
 }
