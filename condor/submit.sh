ac141ab402d9ce4c2475d0ed0afe807a96bcae1d
deoache <daniel.ocampoh@udea.edu.co>
2024-05-17 18:49:13 +0000

    add REQUESTTIMEOUT

--- condor/submit.sh
+++ condor/submit.sh
@@ -3,6 +3,7 @@
 export XRD_NETWORKSTACK=IPv4
 export XRD_RUNFORKHANDLER=1
 export X509_USER_PROXY=X509PATH
+export XRD_REQUESTTIMEOUT=2400
 voms-proxy-info -all
 voms-proxy-info -all -file X509PATH
 cd MAINDIRECTORY
