b31e3a40b0f0fa287c2dbed153ebd502e88eb2e8
deoache <daniel.ocampoh@udea.edu.co>
2024-06-05 09:43:26 +0000

    add additional X509 env variables

--- condor/submit.sh
+++ condor/submit.sh
@@ -3,8 +3,10 @@
 export XRD_NETWORKSTACK=IPv4
 export XRD_RUNFORKHANDLER=1
 export X509_USER_PROXY=X509PATH
-export XRD_REQUESTTIMEOUT=2400
-voms-proxy-info -all
+export X509_USER_CERT=X509PATH
+export X509_CERT_DIR=/cvmfs/cms.cern.ch/grid/etc/grid-security/certificates
+export X509_VOMS_DIR=/cvmfs/cms.cern.ch/grid/etc/grid-security/vomsdir
+export XRD_REQUESTTIMEOUT=3600
 voms-proxy-info -all -file X509PATH
 cd MAINDIRECTORY
 
