04576560d9bf27d726142fc1a9fffa19e3104bc1
deoache <daniel.ocampoh@udea.edu.co>
2024-10-18 11:52:32 +0000

    add to_dict() and to_yaml() methods

--- analysis/configs/processor_config.py
+++ analysis/configs/processor_config.py
@@ -1,3 +1,6 @@
+import yaml
+
+
 class ProcessorConfig:
     """
     Attributes:
@@ -25,3 +28,18 @@ class ProcessorConfig:
         self.object_selection = object_selection
         self.event_selection = event_selection
         self.histogram_config = histogram_config
+
+    def to_dict(self):
+        """Convert ProcessorConfig to a dictionary."""
+        return {
+            "goldenjson": self.goldenjson,
+            "lumidata": self.lumidata,
+            "hlt_paths": self.hlt_paths,
+            "object_selection": self.object_selection,
+            "event_selection": self.event_selection,
+            "histogram_config": self.histogram_config.to_dict(),
+        }
+
+    def to_yaml(self):
+        """Convert ProcessorConfig to a YAML string."""
+        return yaml.dump(self.to_dict(), sort_keys=False, default_flow_style=False)
