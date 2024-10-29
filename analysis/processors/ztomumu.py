6ad205516941891385a0608eb98792bc191edd53
deoache <daniel.ocampoh@udea.edu.co>
2024-10-29 15:24:29 +0000

    update processors

--- analysis/processors/ztomumu.py
+++ analysis/processors/ztomumu.py
@@ -6,12 +6,12 @@ from coffea import processor
 from coffea.nanoevents import PFNanoAODSchema
 from coffea.lumi_tools import LumiData, LumiList
 from coffea.analysis_tools import Weights, PackedSelection
-from analysis.configs import load_config
+from analysis.configs import ProcessorConfigBuilder
 from analysis.corrections.muon import MuonWeights
 from analysis.corrections.pileup import add_pileup_weight
 from analysis.histograms import HistBuilder, fill_histogram
 from analysis.selections import (
-    object_selector,
+    ObjectSelector,
     get_lumi_mask,
     get_trigger_mask,
     get_trigger_match_mask,
@@ -23,22 +23,27 @@ PFNanoAODSchema.warn_missing_crossrefs = False
 
 class ZToMuMuProcessor(processor.ProcessorABC):
     def __init__(self, year: str):
-
         self.year = year
-        self.processor_config = load_config(
-            config_type="processor", config_name="ztomumu", year=year
-        )
+
+        config_builder = ProcessorConfigBuilder(processor="ztomumu", year=year)
+        self.processor_config = config_builder.build_processor_config()
         self.histogram_config = self.processor_config.histogram_config
         self.histograms = HistBuilder(self.histogram_config).build_histogram()
 
     def process(self, events):
-        # get goldenjson and hlt paths
+        # get golden json, HLT paths and selections
+        year = self.year
         goldenjson = self.processor_config.goldenjson
         hlt_paths = self.processor_config.hlt_paths
+        object_selections = self.processor_config.object_selection
+        event_selections = self.processor_config.event_selection
+
         # check if dataset is MC or Data
         is_mc = hasattr(events, "genWeight")
+
         # initialize output dictionary
         output = {}
+
         # initialize metadata info
         nevents = ak.num(events, axis=0)
         output["metadata"] = {}
@@ -65,12 +70,8 @@ class ZToMuMuProcessor(processor.ProcessorABC):
                 year=self.year,
                 variation="nominal",
                 weights=weights_container,
-                id_wp=self.processor_config.object_selection["muons"]["cuts"][
-                    "muon_id"
-                ],
-                iso_wp=self.processor_config.object_selection["muons"]["cuts"][
-                    "muon_iso"
-                ],
+                id_wp=object_selections["leptons"]["cuts"]["muon_id"],
+                iso_wp=object_selections["leptons"]["cuts"]["muon_iso"],
             )
             muon_weights.add_id_weights()
             muon_weights.add_iso_weights()
@@ -85,28 +86,26 @@ class ZToMuMuProcessor(processor.ProcessorABC):
         # --------------------------------------------------------------
         # Object selection
         # --------------------------------------------------------------
-        objects = object_selector(events, self.processor_config.object_selection)
+        object_selector = ObjectSelector(object_selections, year)
+        objects = object_selector.select_objects(events)
 
         # --------------------------------------------------------------
         # Event selection
         # --------------------------------------------------------------
-        selections = {}
-        for selection, str_mask in self.processor_config.event_selection.items():
-            selections[selection] = eval(str_mask)
-
         event_selection = PackedSelection()
-        event_selection.add_multiple(selections)
-        region_selection = event_selection.all(*selections.keys())
+        for selection, str_mask in event_selections.items():
+            event_selection.add(selection, eval(str_mask))
+        region_selection = event_selection.all(*event_selections.keys())
 
         # save cutflow
         output["metadata"].update({"cutflow": {"initial": sumw}})
         current_selection = []
-        for cut_name in selections.keys():
+        for cut_name in event_selections.keys():
             current_selection.append(cut_name)
             output["metadata"]["cutflow"][cut_name] = ak.sum(
                 weights_container.weight()[event_selection.all(*current_selection)]
             )
-        # save raw and weighted number of events after selection
+        # save raw and weighted number of events after selection to metadata
         final_nevents = dak.sum(region_selection)
         weighted_final_nevents = ak.sum(weights_container.weight()[region_selection])
         output["metadata"].update(
@@ -115,9 +114,9 @@ class ZToMuMuProcessor(processor.ProcessorABC):
                 "raw_final_nevents": final_nevents,
             }
         )
+        # save integrated luminosity (/pb) to metadata
         if not is_mc:
-            # compute integrated luminosity (/pb)
-            lumi_mask = selections["lumimask"]
+            lumi_mask = eval(event_selections["lumimask"])
             lumi_data = LumiData(self.processor_config.lumidata)
             lumi_list = LumiList(
                 events[lumi_mask].run, events[lumi_mask].luminosityBlock
@@ -125,7 +124,7 @@ class ZToMuMuProcessor(processor.ProcessorABC):
             lumi = lumi_data.get_lumi(lumi_list)
             # save luminosity to metadata
             output["metadata"].update({"lumi": lumi})
-            
+
         # --------------------------------------------------------------
         # Histogram filling
         # --------------------------------------------------------------
