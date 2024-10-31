9071077146593aac0a85a74f9f2cb39877a00887
deoache <daniel.ocampoh@udea.edu.co>
2024-10-31 08:37:46 +0000

    update docstring

--- analysis/corrections/utils.py
+++ analysis/corrections/utils.py
@@ -9,13 +9,12 @@ POG_JSONS = {
     "electron_scale": ["EGM", "electronSS.json.gz"],
     "jetvetomaps": ["JME", "jetvetomaps.json.gz"],
     "jec": ["JME", "jet_jerc.json.gz"],
-    "ctag": ["BTV", "ctagging.json.gz"]
+    "ctag": ["BTV", "ctagging.json.gz"],
 }
 POG_YEARS = {
     "2022preEE": "2022_Summer22",
     "2022postEE": "2022_Summer22EE",
 }
-
 def get_pog_json(json_name: str, year: str) -> str:
     """
     returns pog json file path
@@ -23,13 +22,12 @@ def get_pog_json(json_name: str, year: str) -> str:
     Parameters:
     -----------
         json_name:
-            json name {pileup, muon, jetvetomaps}
+            pog json name 
         year:
-            dataset year {2022, 2022EE}
+            dataset year {2022preEE, 2022postEE}
     """
     if json_name in POG_JSONS:
         pog_json = POG_JSONS[json_name]
     else:
         print(f"No json for {json_name}")
     return f"{POG_CORRECTION_PATH}/POG/{pog_json[0]}/{POG_YEARS[year]}/{pog_json[1]}"
-
