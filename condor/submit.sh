dd962d23818eeab34b7cb58f5237b99d7831b91d
deoache <daniel.ocampoh@udea.edu.co>
2024-11-12 14:41:42 +0000

    update singularity image

--- condor/submit.sh
+++ condor/submit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-singularity exec -B /cvmfs -B /pnfs -B /user -B /scratch /cvmfs/unpacked.cern.ch/registry.hub.docker.com/coffeateam/coffea-base-almalinux8:0.7.22-py3.8 EXECUTABLEPATH/to_run_JOBNAME.sh
+singularity exec -B /cvmfs -B /pnfs -B /user -B /scratch /cvmfs/unpacked.cern.ch/registry.hub.docker.com/coffeateam/coffea-dask:0.7.22-py3.9-g7f049 EXECUTABLEPATH/to_run_JOBNAME.sh
