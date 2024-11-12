e83bb13c7569c273c42eb6366e7b4e43ca328b67
deoache <daniel.ocampoh@udea.edu.co>
2024-11-12 15:57:17 +0000

    add get_rootfiles()

--- analysis/filesets/utils.py
+++ analysis/filesets/utils.py
@@ -1,11 +1,34 @@
-def divide_list(lst: list) -> list:
+import glob
+import yaml
+from pathlib import Path
+
+
+def get_rootfiles(year: str, dataset: str):
+    main_dir = Path.cwd()
+    fileset_path = Path(f"{main_dir}/analysis/filesets")
+    with open(f"{fileset_path}/{year}_fileset.yaml", "r") as f:
+        dataset_config = yaml.safe_load(f)[dataset]
+
+    # check for .root files in the specified path
+    root_files = glob.glob(f"{dataset_config['path']}/*.root")
+    if not root_files:
+        # if no files found, check in the subdirectories
+        root_files = glob.glob(f"{dataset_config['path']}/*/*.root")
+    elif not root_files:
+        raise FileNotFoundError(
+            f"No .root files found in {dataset_config['path']} or its subdirectories."
+        )
+    return root_files
+
+
+def divide_list(lst: list, nfiles: int = 20) -> list:
     """Divide a list into sublists such that each sublist has at least 20 elements."""
-    if len(lst) < 20:
+    if len(lst) < nfiles:
         return [lst]
 
     # Dynamically calculate the number of sublists such that each has at least 20 elements
-    n = len(lst) // 20  # This gives the number of groups with at least 20 elements
-    if len(lst) % 20 != 0:
+    n = len(lst) // nfiles  # This gives the number of groups with at least 20 elements
+    if len(lst) % nfiles != 0:
         n += 1  # Increase n by 1 if there is a remainder, to accommodate extra elements
 
     # Divide the list into 'n' sublists
