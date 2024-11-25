d9350439087edcc0b3eafeac47d4ea56a9c3df68
deoache <daniel.ocampoh@udea.edu.co>
2024-11-25 10:30:44 +0000

    update filler to use Axis object attributes

--- analysis/histograms/hist_filler.py
+++ analysis/histograms/hist_filler.py
@@ -9,69 +9,67 @@ def normalize(array: ak.Array):
         return ak.fill_none(array, np.nan)
 
 
-def get_flow_array(histogram, feature, feature_map):
-    histogram_edges = histogram.axes[feature].edges
+def get_flow_array(histogram, variable, variables_map):
+    histogram_edges = histogram.axes[variable].edges
     epsilon = (histogram_edges[-1] - histogram_edges[-2]) / 2
     hist_max_bin_edge = histogram_edges[-1] - epsilon
     hist_min_bin_edge = histogram_edges[0]
-
     return np.maximum(
-        np.minimum(normalize(feature_map[feature]), hist_max_bin_edge),
+        np.minimum(normalize(variables_map[variable]), hist_max_bin_edge),
         hist_min_bin_edge,
     )
 
 
-def get_variable_array(histogram, histogram_config, feature, feature_map, flow):
-    if histogram_config.axes[feature]["type"] == "IntCategory":
-        variable_array = normalize(feature_map[feature])
-        # convert to integer array
+def get_variable_array(histogram, histogram_config, variable, variables_map, flow):
+    if histogram_config.axes[variable].type == "IntCategory":
+        # cast to integer array
+        variable_array = normalize(variables_map[variable])
         variable_array = ak.to_numpy(variable_array).astype(int)
     elif flow:
         # add underflow/overflow to first/last bin
         variable_array = get_flow_array(
             histogram=histogram,
-            feature=feature,
-            feature_map=feature_map,
+            variable=variable,
+            variables_map=variables_map,
         )
     else:
-        variable_array = normalize(feature_map[feature])
-
+        variable_array = normalize(variables_map[variable])
     return variable_array
 
 
 def fill_histogram(
-    histograms, histogram_config, feature_map, category, weights, variation, flow=True
+    histograms, histogram_config, variables_map, category, weights, variation, flow=True
 ):
     if histogram_config.layout == "individual":
-        for feature in histograms:
+        for variable in histograms:
             variable_array = get_variable_array(
-                histograms[feature], histogram_config, feature, feature_map, flow
+                histograms[variable], histogram_config, variable, variables_map, flow
             )
             fill_args = {
-                feature: variable_array,
+                variable: variable_array,
                 "variation": variation,
                 "category": category,
                 "weight": (
-                    ak.flatten(ak.ones_like(feature_map[feature]) * weights)
-                    if feature_map[feature].ndim == 2
+                    ak.flatten(ak.ones_like(variables_map[variable]) * weights)
+                    if variables_map[variable].ndim == 2
                     else weights
                 ),
             }
-            histograms[feature].fill(**fill_args)
+            histograms[variable].fill(**fill_args)
     else:
-        for key, features in histogram_config.layout.items():
+        for key, variables in histogram_config.layout.items():
             fill_args = {}
-            for feature in features:
-                fill_args[feature] = get_variable_array(
-                    histograms[key], histogram_config, feature, feature_map, flow
+            for variable in variables:
+                fill_args[variable] = get_variable_array(
+                    histograms[key], histogram_config, variable, variables_map, flow
                 )
             fill_args.update(
                 {
                     "variation": variation,
                     "category": category,
                     "weight": (
-                        ak.flatten(ak.ones_like(feature_map[feature]) * weights)
-                        if feature_map[feature].ndim == 2
+                        ak.flatten(ak.ones_like(variables_map[variable]) * weights)
+                        if variables_map[variable].ndim == 2
                         else weights
                     ),
                 }
