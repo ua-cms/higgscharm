e2bd2c6f245f95510764e862fdbd3dd46e34fca4
deoache <daniel.ocampoh@udea.edu.co>
2024-11-25 11:52:00 +0000

    add 'hadronFlavour' for data samples

--- analysis/processors/ztomumu.py
+++ analysis/processors/ztomumu.py
@@ -157,6 +157,8 @@ class ZToMuMuProcessor(processor.ProcessorABC):
             # --------------------------------------------------------------
             if nevents_after > 0:
                 # get analysis variables
+                if not is_mc:
+                    events["Jet", "hadronFlavour"] = ak.zeros_like(events.Jet.pt)
                 variables_map = {}
                 for variable, axis in self.histogram_config.axes.items():
                     variables_map[variable] = eval(axis.expression)[category_mask]
