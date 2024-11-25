a3323f77c946d6329f462c79522a29ff6e8eb819
deoache <daniel.ocampoh@udea.edu.co>
2024-11-25 10:28:47 +0000

    change histogram building from axis config to axis name

--- analysis/histograms/hist_builder.py
+++ analysis/histograms/hist_builder.py
@@ -3,6 +3,7 @@ import hist
 
 class HistBuilder:
     def __init__(self, processor_config):
+        self.processor_config = processor_config
         self.histogram_config = processor_config.histogram_config
         self.axis_opt = {
             "StrCategory": hist.axis.StrCategory,
@@ -10,60 +11,47 @@ class HistBuilder:
             "Regular": hist.axis.Regular,
             "Variable": hist.axis.Variable,
         }
-        # define systematic variations axis
-        self.syst_axis = self.build_axis(
-            {"variation": {"type": "StrCategory", "categories": [], "growth": True}}
-        )
-        # define category axis
-        categories = list(processor_config.event_selection["categories"].keys())
-        self.category_axis = self.build_axis(
-            {"category": {"type": "StrCategory", "categories": categories}}
-        )
-
-    def build_axis(self, axis_config: dict):
-        """build a hist axis object from an axis config"""
-        axis_args = {}
-        for name in axis_config:
-            axis_args["name"] = name
-            hist_type = axis_config[name]["type"]
-            for arg_name, arg_value in axis_config[name].items():
-                if arg_name in ["type", "expression"]:
-                    continue
-                axis_args[arg_name] = arg_value
-        hist_args = {k: v for k, v in axis_args.items()}
-        axis = self.axis_opt[hist_type](**hist_args)
-        return axis
-
-    def build_individual_histogram(self):
-        histograms = {}
-        for name, args in self.histogram_config.axes.items():
-            axes = [self.build_axis({name: args})]
-            axes.append(self.category_axis)
-            if self.histogram_config.add_syst_axis:
-                axes.append(self.syst_axis)
-            if self.histogram_config.add_weight:
-                axes.append(hist.storage.Weight())
-            histograms[name] = hist.Hist(*axes)
-        return histograms
-
-    def build_stacked_histogram(self, axes_names):
-        axes = []
-        for name, args in self.histogram_config.axes.items():
-            if name in axes_names:
-                axes.append(self.build_axis({name: args}))
-        axes.append(self.category_axis)
-        if self.histogram_config.add_syst_axis:
-            axes.append(self.syst_axis)
-        if self.histogram_config.add_weight:
-            axes.append(hist.storage.Weight())
-        histograms = hist.Hist(*axes)
-        return histograms
 
     def build_histogram(self):
-        if self.histogram_config.layout == "individual":
-            histograms = self.build_individual_histogram()
-        else:
+        if self.histogram_config.stack:
             histograms = {}
             for hist_name, axes_names in self.histogram_config.layout.items():
                 histograms[hist_name] = self.build_stacked_histogram(axes_names)
+        else:
+            histograms = self.build_individual_histogram()
         return histograms
+
+    def build_individual_histogram(self):
+        histograms = {}
+        for axis in self.histogram_config.axes:
+            axes = [self.build_axis(axis)]
+            axes.append(self.get_category_axis())
+            if self.histogram_config.add_syst_axis:
+                axes.append(self.get_syst_axis())
+            if self.histogram_config.add_weight:
+                axes.append(hist.storage.Weight())
+            histograms[axis] = hist.Hist(*axes)
+        return histograms
+
+    def build_stacked_histogram(self, axes_names):
+        axes = [self.get_category_axis()]
+        for axis in axes_names:
+            axes.append(self.build_axis(axis))
+        if self.histogram_config.add_syst_axis:
+            axes.append(self.get_syst_axis())
+        if self.histogram_config.add_weight:
+            axes.append(hist.storage.Weight())
+        return hist.Hist(*axes)
+
+    def build_axis(self, axis_name: dict):
+        """build a hist axis object from an axis config"""
+        hist_type = self.histogram_config.axes[axis_name].type
+        axis_args = self.histogram_config.axes[axis_name].build_args
+        return self.axis_opt[hist_type](**axis_args)
+
+    def get_syst_axis(self):
+        return hist.axis.StrCategory(name="variation", categories=[], growth=True)
+
+    def get_category_axis(self):
+        categories = list(self.processor_config.event_selection["categories"].keys())
+        return hist.axis.StrCategory(name="category", categories=categories)
