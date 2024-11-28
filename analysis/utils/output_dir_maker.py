e3d220720df5e6ec9e833767572ad35f5e03ed52
deoache <daniel.ocampoh@udea.edu.co>
2024-11-28 10:14:45 +0000

    add output dir maker

---
+++ analysis/utils/output_dir_maker.py
@@ -0,0 +1,10 @@
+from analysis.utils.path_handler import Paths
+
+def make_output_directory(args: dict) -> str:
+    """builds output directories. Returns output path"""
+    paths = Paths(eos=args["eos"])
+    processor_output_path = paths.processor_path(
+        processor=args["processor"],
+        year=args["year"],
+    )
+    return str(processor_output_path)
