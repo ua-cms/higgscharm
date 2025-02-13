8e1e9f32686b40da393fbea15e10288d3fc26ba5
deoache <daniel.ocampoh@udea.edu.co>
2025-02-13 15:57:14 +0000

    update

--- runner.py
+++ runner.py
@@ -1,20 +1,71 @@
 import os
 import argparse
-from analysis.configs import ProcessorConfigBuilder
 
 
-def get_datasets(processor: str, year: str):
-    eras = {"2022preEE": ["C", "D"], "2022postEE": ["E", "F", "G"]}
-    config_builder = ProcessorConfigBuilder(processor=processor, year=year)
-    processor_config = config_builder.build_processor_config()
-    mc_datasets = processor_config.mc_datasets
-    primary_datasets = list(processor_config.hlt_paths.keys())
-    data_datasets = []
-    for primary_dataset in primary_datasets:
-        for era in eras[year]:
-            data_datasets.append(f"{primary_dataset}{era}")
+ERAS = {"2022preEE": ["C", "D"], "2022postEE": ["E", "F", "G"]}
+PRIMARY_DATASETS = ["Muon", "MuonEG", "EGamma"]
+DATA_SAMPLES = {}
+for year, eras in ERAS.items():
+    DATA_SAMPLES[year] = {}
+    for primary_dataset in PRIMARY_DATASETS:
+        DATA_SAMPLES[year][primary_dataset] = []
+        for era in eras:
+            DATA_SAMPLES[year][primary_dataset].append(f"{primary_dataset}{era}")
 
-    return data_datasets + mc_datasets
+            
+MC_DATASETS = {
+    "ttbar": ["TTto2L2Nu", "TTto4Q", "TTtoLNu2Q"],
+    "singletop": [
+        "TWminusto2L2Nu",
+        "TbarWplusto2L2Nu",
+        "TWminustoLNu2Q",
+        "TbarWplusto4Q",
+        "TbarWplustoLNu2Q",
+        "TWminusto4Q",
+        "TbarBQ",
+        "TBbarQ",
+    ],
+    "diboson": ["WW", "WZ", "ZZ"],
+    "dyjets": ["DYto2L_2Jets_50", "DYto2L_2Jets_10to50"],
+    "higgs": [
+        "bbH_Hto2Zto4L",
+        "GluGluHtoZZto4L",
+        "TTH_Hto2Z",
+        "VBFHto2Zto4L",
+        "WminusH_Hto2Zto4L",
+        "WplusH_Hto2Zto4L",
+        "ZHto2Zto4L",
+    ],
+    "ggtozz": [
+        "GluGluToContinto2Zto2E2Mu",
+        "GluGluToContinto2Zto2E2Tau",
+        "GluGluToContinto2Zto2Mu2Tau",
+        "GluGlutoContinto2Zto4E",
+        "GluGlutoContinto2Zto4Mu",
+        "GluGlutoContinto2Zto4Tau",
+    ],
+    "qqtozz": ["ZZto4L"],
+}
+
+
+DATASETS = {
+    "hww": {
+        "mc": ["ttbar", "singletop", "diboson"],
+        "data": ["Muon", "MuonEG", "EGamma"],
+    },
+    "zzto4l": {
+        "mc": ["higgs", "ggtozz", "qqtozz"],
+        "data": ["Muon", "MuonEG", "EGamma"],
+    },
+    "ztoee": {
+        "mc": ["ttbar", "singletop", "diboson"], 
+        "data": ["EGamma"]
+    },
+    "ztomumu": {
+        "mc": ["ttbar", "singletop", "diboson"], 
+        "data": ["EGamma"]
+    },
+}
 
 
 if __name__ == "__main__":
@@ -49,8 +100,20 @@ if __name__ == "__main__":
         help="Enable saving outputs to /eos",
     )
     args = parser.parse_args()
-
-    for dataset in get_datasets(processor=args.processor, year=args.year):
+    # get datasets for processor and year
+    mc = [
+        sample
+        for dataset in DATASETS[args.processor]["mc"]
+        for sample in MC_DATASETS[dataset]
+    ]
+    data = [
+        sample
+        for dataset in DATASETS[args.processor]["data"]
+        for sample in DATA_SAMPLES[args.year][dataset]
+    ]
+    datasets = mc + data
+    # submit job for each dataset
+    for dataset in datasets:
         cmd = f"python3 submit_condor.py --processor {args.processor} --year {args.year} --dataset {dataset} --nfiles {args.nfiles}"
         if args.submit:
             cmd += " --submit"
