a9dbf80e4ee1ab08846f19069e0b45ae1b99a257
deoache <daniel.ocampoh@udea.edu.co>
2025-02-21 21:02:09 +0000

    update

--- analysis/utils/output_dir_maker.py
+++ analysis/utils/output_dir_maker.py
@@ -1,11 +1,14 @@
 from analysis.utils.path_handler import Paths
 
+
 def make_output_directory(args: dict) -> str:
     """builds output directories. Returns output path"""
     paths = Paths(eos=args["eos"])
-    processor_output_path = paths.processor_path(
-        processor=args["processor"],
-        year=args["year"],
-        dataset=args["dataset"]
-    )
+    path_args = {}
+    for arg in ["processor", "year", "dataset"]:
+        if arg in args:
+            path_args[arg] = args[arg]
+        else:
+            path_args[arg] = None
+    processor_output_path = paths.processor_path(**path_args)
     return str(processor_output_path)
