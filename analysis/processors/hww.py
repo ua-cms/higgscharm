88408cdd59e11446b6075be2b281bc4fc7c4f3ef
deoache <daniel.ocampoh@udea.edu.co>
2025-02-26 19:54:36 +0000

    update for 2023

--- analysis/processors/hww.py
+++ analysis/processors/hww.py
@@ -7,7 +7,6 @@ from coffea.lumi_tools import LumiData, LumiList
 from coffea.analysis_tools import Weights, PackedSelection
 from coffea.nanoevents.methods.vector import LorentzVector
 from analysis.configs import ProcessorConfigBuilder
-from analysis.filesets.utils import get_dataset_name
 from analysis.histograms import HistBuilder, fill_histogram
 from analysis.corrections.muon import MuonWeights
 from analysis.corrections.pileup import add_pileup_weight
@@ -35,7 +34,6 @@ class HWWProcessor(processor.ProcessorABC):
 
     def process(self, events):
         dataset = events.metadata["dataset"]
-        dataset_key = get_dataset_name(dataset)
         # get golden json, triggers, selections and histograms
         year = self.year
         goldenjson = self.processor_config.goldenjson
@@ -75,21 +73,24 @@ class HWWProcessor(processor.ProcessorABC):
             apply_junc=apply_junc,
         )
         # apply electron scale and smearing corrections
-        electron_ss = ElectronSS(
-            events=events,
-            year=year,
-            variation="nominal",
-        )
-        if is_mc:
-            # energies in MC are smeared
-            electron_ss.apply_smearing()
+        if year.startswith("2022"):
+            electron_ss = ElectronSS(
+                events=events,
+                year=year,
+                variation="nominal",
+            )
+            if is_mc:
+                # energies in MC are smeared
+                electron_ss.apply_smearing()
+            else:
+                # energies in data are scaled
+                electron_ss.apply_scale()
         else:
-            # energies in data are scaled
-            electron_ss.apply_scale()
+            events["Electron", "pt_raw"] = events.Electron.pt
 
         # apply MET-phi modulation corrections
-        apply_met_phi_corrections(events, is_mc, year)
-
+        if year.startswith("2022"):
+            apply_met_phi_corrections(events, is_mc, year)
         # --------------------------------------------------------------
         # Weights
         # --------------------------------------------------------------
