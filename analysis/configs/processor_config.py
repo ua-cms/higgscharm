876bbe38f639cc3641763fb56688fa3ca4e49c0f
deoache <daniel.ocampoh@udea.edu.co>
2025-03-16 11:04:46 +0000

    update processor config
    - Add 'add_cut' option for objects

--- analysis/configs/processor_config.py
+++ analysis/configs/processor_config.py
@@ -1,13 +1,9 @@
 import yaml
 
-
 class ProcessorConfig:
     """
     Attributes:
     -----------
-        goldenjson:
-        lumidata:
-        hlt_paths:
         object_selection:
         event_selection:
         histogram_config:
@@ -15,16 +11,10 @@ class ProcessorConfig:
 
     def __init__(
         self,
-        goldenjson,
-        lumidata,
-        hlt_paths,
         object_selection,
         event_selection,
         histogram_config,
     ):
-        self.goldenjson = goldenjson
-        self.lumidata = lumidata
-        self.hlt_paths = hlt_paths
         self.object_selection = object_selection
         self.event_selection = event_selection
         self.histogram_config = histogram_config
@@ -32,9 +22,6 @@ class ProcessorConfig:
     def to_dict(self):
         """Convert ProcessorConfig to a dictionary."""
         return {
-            "goldenjson": self.goldenjson,
-            "lumidata": self.lumidata,
-            "hlt_paths": self.hlt_paths,
             "object_selection": self.object_selection,
             "event_selection": self.event_selection,
             "histogram_config": self.histogram_config.to_dict(),
--- analysis/configs/processor_config_builder.py
+++ analysis/configs/processor_config_builder.py
@@ -5,36 +5,42 @@ from analysis.configs.processor_config import ProcessorConfig
 
 
 class ProcessorConfigBuilder:
-    
+
     def __init__(self, processor: str, year: str):
-        with importlib.resources.open_text(f"analysis.configs.{processor}", f"{year}.yaml") as file:
+        with importlib.resources.open_text(
+            f"analysis.configs.{processor}", f"{processor}.yaml"
+        ) as file:
             self.config = yaml.safe_load(file)
-            
+
     def build_processor_config(self):
         return ProcessorConfig(
-            goldenjson=self.config["goldenjson"],
-            lumidata=self.config["lumidata"],
-            hlt_paths=self.config["hlt_paths"],
             object_selection=self.parse_object_selection(),
             event_selection=self.parse_event_selection(),
             histogram_config=self.parse_histogram_config(),
         )
-            
+
     def parse_object_selection(self):
         object_selection = {}
-        for object_name in self.config['object_selection']:
-            object_selection[object_name] = {"field": self.config['object_selection'][object_name]["field"]}
-            if self.config['object_selection'][object_name]["cuts"]:
-                object_selection[object_name]["cuts"] = {}
-                for cut_name, cut in self.config['object_selection'][object_name]["cuts"].items():
-                    object_selection[object_name]["cuts"][cut_name] = cut
+        for object_name in self.config["object_selection"]:
+            object_selection[object_name] = {
+                "field": self.config["object_selection"][object_name]["field"]
+            }
+            if "cuts" in self.config["object_selection"][object_name]:
+                object_selection[object_name]["cuts"] = self.config["object_selection"][
+                    object_name
+                ]["cuts"]
+            if "add_cut" in self.config["object_selection"][object_name]:
+                cuts_to_add = self.config["object_selection"][object_name]["add_cut"]
+                object_selection[object_name]["add_cut"] = {}
+                for cut_name, cuts in cuts_to_add.items():
+                    object_selection[object_name]["add_cut"][cut_name] = cuts
         return object_selection
-    
+
     def parse_event_selection(self):
         event_selection = {}
-        for cut_name, cut in self.config['event_selection'].items():
+        for cut_name, cut in self.config["event_selection"].items():
             event_selection[cut_name] = cut
         return event_selection
-    
+
     def parse_histogram_config(self):
         return HistogramConfig(**self.config["histogram_config"])
