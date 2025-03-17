a4d7721c0e6a9cd3dc9e43b1e1a2d0eaf3655e79
deoache <daniel.ocampoh@udea.edu.co>
2025-03-17 17:41:35 +0000

    add base processor

---
+++ analysis/processors/base.py
@@ -0,0 +1,147 @@
+import numpy as np
+import awkward as ak
+from copy import deepcopy
+from coffea import processor
+from coffea.nanoevents import NanoAODSchema
+from coffea.analysis_tools import Weights, PackedSelection
+from coffea.nanoevents.methods.vector import LorentzVector
+from analysis.utils import dump_lumi
+from analysis.configs import ProcessorConfigBuilder
+from analysis.histograms import HistBuilder, fill_histograms
+from analysis.corrections.correction_manager import (
+    object_corrector_manager,
+    weight_manager,
+)
+from analysis.selections import (
+    ObjectSelector,
+    get_lumi_mask,
+    get_trigger_mask,
+    get_zzto4l_trigger_mask,
+    get_metfilters_mask,
+)
+
+
+NanoAODSchema.warn_missing_crossrefs = False
+
+
+class BaseProcessor(processor.ProcessorABC):
+    def __init__(self, processor: str, year: str):
+        self.year = year
+
+        config_builder = ProcessorConfigBuilder(
+            processor=processor, year="2022" if year.startswith("2022") else "2023"
+        )
+        self.processor_config = config_builder.build_processor_config()
+        self.histogram_config = self.processor_config.histogram_config
+        self.histograms = HistBuilder(self.processor_config).build_histogram()
+
+    def process(self, events):
+        year = self.year
+        dataset = events.metadata["dataset"]
+
+        object_selections = self.processor_config.object_selection
+        event_selection = self.processor_config.event_selection
+        hlt_paths = event_selection["hlt_paths"]
+        histograms = deepcopy(self.histograms)
+
+        # check if dataset is MC or Data
+        is_mc = hasattr(events, "genWeight")
+        if not is_mc:
+            events["Jet", "hadronFlavour"] = ak.zeros_like(events.Jet.pt)
+
+        # initialize output dictionary
+        output = {}
+
+        # initialize metadata info with sumw before selection
+        output["metadata"] = {}
+        sumw = ak.sum(events.genWeight) if is_mc else len(events)
+        output["metadata"].update({"sumw": sumw})
+
+        # --------------------------------------------------------------
+        # Object corrections
+        # --------------------------------------------------------------
+        object_corrector_manager(
+            events=events,
+            year=year,
+            dataset=dataset,
+            processor_config=self.processor_config,
+        )
+
+        # --------------------------------------------------------------
+        # Object selection
+        # --------------------------------------------------------------
+        object_selector = ObjectSelector(object_selections, year)
+        objects = object_selector.select_objects(events)
+
+        # --------------------------------------------------------------
+        # Event selection
+        # --------------------------------------------------------------
+        if not is_mc:
+            # save (run, luminosityBlock) pairs to metadata
+            lumi_mask = eval(event_selection["selections"]["lumimask"])
+            dump_lumi(events[lumi_mask], output)
+
+        # initialize selection manager
+        selection_manager = PackedSelection()
+        # add all selections to selector manager
+        for selection, mask in event_selection["selections"].items():
+            selection_manager.add(selection, eval(mask))
+
+        # --------------------------------------------------------------
+        # Histogram filling
+        # --------------------------------------------------------------
+        categories = event_selection["categories"]
+        for category, category_cuts in categories.items():
+            # get selection mask by category
+            category_mask = selection_manager.all(*category_cuts)
+            # get pruned events
+            pruned_ev = events[category_mask]
+            # add each selected object to 'pruned_ev' as a new field
+            for obj in objects:
+                pruned_ev[f"selected_{obj}"] = objects[obj][category_mask]
+            # get weights container
+            weights_container = weight_manager(
+                pruned_ev=pruned_ev,
+                year=year,
+                dataset=dataset,
+                processor_config=self.processor_config,
+            )
+            # save cutflow to metadata
+            output["metadata"][category] = {"cutflow": {"initial": len(events)}}
+            selections = []
+            for cut_name in category_cuts:
+                selections.append(cut_name)
+                current_selection = selection_manager.all(*selections)
+                output["metadata"][category]["cutflow"][cut_name] = ak.sum(
+                    current_selection
+                )
+            # save number of events after selection to metadata
+            nevents_after = ak.sum(category_mask)
+            weighted_final_nevents = ak.sum(weights_container.weight())
+            output["metadata"][category].update(
+                {
+                    "weighted_final_nevents": weighted_final_nevents,
+                    "raw_final_nevents": nevents_after,
+                }
+            )
+            # get analysis variables and fill histograms
+            if nevents_after > 0:
+                variables_map = {}
+                for variable, axis in self.histogram_config.axes.items():
+                    variables_map[variable] = eval(axis.expression)[category_mask]
+                fill_histograms(
+                    histogram_config=self.histogram_config,
+                    weights_container=weights_container,
+                    variables_map=variables_map,
+                    histograms=histograms,
+                    variation="nominal",
+                    category=category,
+                    is_mc=is_mc,
+                    flow=True,
+                )
+        # add histograms to output dictionary
+        output["histograms"] = histograms
+        return output
+
+    def postprocess(self, accumulator):
+        pass
