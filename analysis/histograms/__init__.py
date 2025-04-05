31ac37ccd7e885e4ca854ad600002cabcfe4adab
deoache <daniel.ocampoh@udea.edu.co>
2025-04-05 13:47:57 +0000

    import IntegerAxis

--- analysis/histograms/__init__.py
+++ analysis/histograms/__init__.py
@@ -1,3 +1,3 @@
 from analysis.histograms.hist_builder import HistBuilder as HistBuilder
 from analysis.histograms.hist_filler import fill_histograms as fill_histograms
-from analysis.histograms.histogram_config import VariableAxis, RegularAxis, IntCategoryAxis, StrCategoryAxis, HistogramConfig
+from analysis.histograms.histogram_config import VariableAxis, RegularAxis, IntCategoryAxis, IntegerAxis, StrCategoryAxis, HistogramConfig
